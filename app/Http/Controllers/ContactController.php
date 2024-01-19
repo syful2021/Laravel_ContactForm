bf611e3c78aba726c250a1f9215e58c3b1d4df31
syful2021 <mdsyfulislam20009@gmail.com>
2024-01-19 17:06:13 +0600

    created: controller,route,contact frontend file and email.contact blade

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\ContactUs;
 
 class ContactController extends Controller
 {
-    
+    public function show()
+    {
+        return view('contact');
+    }
+
+    public function send()
+    {
+        $data = request()->validate([
+            'name' => 'required|min:3',
+            'email' => 'required|email',
+            'message' => 'required|min:5',
+        ]);
+        Mail::to('receipentemail@gmail.com')->send(new ContactUs($data));
+
+        dd('sent');
+
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//  Start
+
+Route::get('/contact', [ContactController::class, 'show'])->name('contact.show');
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
+
+
+    // --------------
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -29,3 +39,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
